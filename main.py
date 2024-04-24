17c9bdef32087e18aaed8c82f954ad64af3bfbd0
AndrPerep <79010353+AndrPerep@users.noreply.github.com>
2024-04-24 13:52:39 +0300

    Add `send_picture`

--- main.py
+++ main.py
@@ -15,40 +15,53 @@ with open("messages.json", "r", encoding="utf-8") as file:
 
 
 # Ответ на сообщение
-def get_keyboard(message, menu):
+def get_keyboard(message, answer):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    if menu["type"] != "Назад":
+    if answer["type"] != "main":
         keyboard.add("Назад")
-    for button in menu["buttons"]:
+    for button in answer["buttons"]:
         keyboard.add(button)
     return keyboard
+
+
+# Отправка шпаргалок
+def send_picture(message):
+    try:
+        with open(f'pictures/{message.text}.jpg', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+    except FileNotFoundError:
+        bot.reply_to(message, "Этой шпаргалки пока нет")
 
 
 # Обработчик команды /start или текста "Привет"
 @bot.message_handler(commands=['start', 'help'])
 @bot.message_handler(func=lambda message: message.text.lower() == 'привет')
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я бот. Чем могу помочь?", reply_markup=get_keyboard(message, menu=ANSWERS[message.text]))
+    bot.reply_to(message, "Привет! Я бот. Чем могу помочь?", reply_markup=get_keyboard(message, answer=ANSWERS[message.text]))
 
 
 # Обработчик команды Назад
 @bot.message_handler(func=lambda message: message.text.lower() == 'назад')
 def handle_back(message):
-    bot.reply_to(message, "Вы вернулись в главное меню.", reply_markup=get_keyboard(message, menu=ANSWERS[message.text]))
+    bot.reply_to(message, "Вы вернулись в главное меню.", reply_markup=get_keyboard(message, answer=ANSWERS[message.text]))
 
 # Обработчик нажатия на кнопки главного меню
 @bot.message_handler(func=lambda message: True)
 def handle_main_menu(message):
     if message.text in ANSWERS.keys():
         answer = ANSWERS[message.text]
-        if answer.get('buttons'):
+        message_type = answer['type']
+        if message_type == 'keyboard':
             bot.reply_to(
                 message,
                 "Выберите пункт меню:",
-                reply_markup=get_keyboard(message, menu=ANSWERS[message.text])
+                reply_markup=get_keyboard(message, answer=answer)
             )
-        else:
-            bot.reply_to(message, answer["text"])
+        elif message_type == 'texts':
+            for text in answer["texts"]:
+                bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    elif "📝" in message.text:
+        send_picture(message)
     else:
         bot.reply_to(message, "Я вас не понимаю. Используйте кнопки на клавиатуре")
 
